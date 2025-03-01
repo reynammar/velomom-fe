9db1fb1319d0c2827e648fa7854f12eee944a18f
Arif Ammar <ammarreynarif@gmail.com>
2025-03-01 11:14:35 +0700

    memulai untuk membuat landing page

--- velomom-app/src/main.tsx
+++ velomom-app/src/main.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css';
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
+import HomePage from "./pages/home";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
---
+++ velomom-app/src/pages/home.tsx
@@ -0,0 +1,10 @@
+const HomePage: React.FC = () => {
+    return (
+        <div className="flex justify-center items-center min-h-screen w-full">
+            <h1>Halo kamu sedang ada di home</h1>
+        </div>
+    );
+}
+
+
+export default HomePage
